b93db15485b7418552928af553c2844748eb0031
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2023-01-15 00:28:39 +0100

    Fix

--- hass-listener/function/handler.rb
+++ hass-listener/function/handler.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
 
 class Handler
-  def run(body, env)
+  def run(body, headers)
     status_code = 200 # Optional status code, defaults to 200
     response_headers = {"content-type" => "text/plain"}
     body = "Hello world from the Ruby template"
 
-    [body, response_headers, status_code]
+    return body, response_headers, status_code
   end
 end
 
