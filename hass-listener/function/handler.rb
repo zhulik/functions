11855f09220d21d716131fe003a8be4699e0a9d6
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2023-01-14 23:51:48 +0100

    https mode

--- hass-listener/function/handler.rb
+++ hass-listener/function/handler.rb
@@ -1,12 +1,22 @@
 class Handler
-  def run(req)
-    return "Hello world from the Ruby template"
+  def run(body, env)
+    status_code = 200 # Optional status code, defaults to 200
+    response_headers = {"content-type" => "text/plain"}
+    body = "Hello world from the Ruby template"
+
+    [body, response_headers, status_code]
   end
 end
 
-req = ARGF.read
+require 'sinatra'
+
+set :port, 5000
+# set :bind, '0.0.0.0'
 
 handler = Handler.new
-res = handler.run req
 
-puts res
+get '/*' do
+  res, res_headers, status = handler.run(request.body, request.env)
+
+  [status || 200, res_headers, res]
+end
