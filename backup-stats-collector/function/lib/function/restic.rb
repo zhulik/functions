b6a74abc0eeaf811afdff2f033a5995d86cce5fb
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2023-01-15 16:46:36 +0100

    Fix

--- backup-stats-collector/function/lib/function/restic.rb
+++ backup-stats-collector/function/lib/function/restic.rb
@@ -3,6 +3,12 @@
 class Function::Restic
   def stats
     # restic gets it's arguments directly from env
-    JSON.parse(Async::Process.capture("restic", "snapshots"), symbolize_names: true)
+    JSON.parse(snapshots, symbolize_names: true)
+  end
+
+  private
+
+  def snapshots
+    Async::Process.capture("restic", "snapshots", "--no-lock", "--json")
   end
 end
