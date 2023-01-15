92cdee5464e68ce7f34212901af41d008dbda12c
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2023-01-15 18:26:29 +0100

    Fix

--- backup-stats-collector/function/spec/spec_helper.rb
+++ backup-stats-collector/function/spec/spec_helper.rb
@@ -3,6 +3,7 @@
 ENV["ENV"] ||= "test"
 ENV["AUTH_TOKEN"] ||= "token"
 
+ENV["AWS_REGION"] ||= "main"
 ENV["MINIO_HOST"] ||= "minio.example.com"
 ENV["MINIO_BUCKET"] ||= "bucket"
 ENV["LAST_VERIFIED_PATH"] ||= "last_verified"
