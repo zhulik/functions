493a6cd713354b39a337f029e89a35e0d942b520
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2023-01-18 00:28:00 +0100

    Fix

--- time-passed/function/lib/function/handler.rb
+++ time-passed/function/lib/function/handler.rb
@@ -3,7 +3,7 @@
 class Function::Handler
   AUTH_TOKEN = ENV.fetch("AUTH_TOKEN")
   BUCKET = ENV.fetch("BUCKET")
-  PATH = ENV.fetch("PATH")
+  PATH = ENV.fetch("FILE_PATH")
   MINIO_HOST = ENV.fetch("MINIO_HOST")
   METRICS_PREFIX = ENV.fetch("METRICS_PREFIX")
 
